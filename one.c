db43897ab9da71b34164806820bd1c91cd646e78
fahad2310 <siddiquifahad256@gmail.com>
2022-11-02 16:28:01 +0530

    initial commit

--- one.c
+++ one.c
@@ -4,6 +4,11 @@ void main()
 {       
     printf("Hello World!");
     printf("My name is fahad");
-    printf("HI fahad");
+    printf("HI ayushi");
+    //kaisa hai fahad//
+    //chal github seekhe//
+    //chalega?//
+    //haa...//
+    
 
 }
